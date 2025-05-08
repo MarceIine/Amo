05b224597b525bfd6f40487558773238b2f98f96
MarceIine <ac405040500@gmail.com>
2025-05-08 19:27:28 -0400

    Added keybinds to quit the game.

--- src/main.lua
+++ src/main.lua
@@ -9,49 +9,55 @@ _G.HC = require("HC")
 local collisionText = ""
 local objects = {}
 
+function love.keypressed(key, scancode, isrepeat)
+	if key == "escape" or key == "q" then
+		love.event.quit()
+	end
+end
+
 function love.load()
-    local coin = love.graphics.newImage("assets/images/coin.png")
-    local size = Vector2.new(80, 80)
-    for index = 1, 10 do
-        local position = Vector2.new(love.math.random(0, 800), love.math.random(0, 600))
-        local newCollectable = Collectable.new(position, size, coin)
-        table.insert(objects, newCollectable)
-    end
-    -- Create player
-    player = Player.new(Vector2.new(80, 80))
+	local coin = love.graphics.newImage("assets/images/coin.png")
+	local size = Vector2.new(80, 80)
+	for index = 1, 10 do
+		local position = Vector2.new(love.math.random(0, 800), love.math.random(0, 600))
+		local newCollectable = Collectable.new(position, size, coin)
+		table.insert(objects, newCollectable)
+	end
+	-- Create player
+	player = Player.new(Vector2.new(80, 80))
 end
 
 function love.update(dt)
-  player:update(dt)
-  
-  collisionText = ""
-  -- Check collisions between player and all objects
-  for name, obj in pairs(objects) do
-    local collides, dx, dy = player.collision:collidesWith(obj.collision)
-    if collides then
-      collisionText = collisionText .. string.format("\nCollision with %s! Delta: %.2f, %.2f", name, dx, dy)
-    table.remove(objects, name)
-    end
-  end
-  
-  if collisionText == "" then
-    collisionText = "No collisions"
-  end
+	player:update(dt)
+
+	collisionText = ""
+	-- Check collisions between player and all objects
+	for name, obj in pairs(objects) do
+		local collides, dx, dy = player.collision:collidesWith(obj.collision)
+		if collides then
+			collisionText = collisionText .. string.format("\nCollision with %s! Delta: %.2f, %.2f", name, dx, dy)
+			table.remove(objects, name)
+		end
+	end
+
+	if collisionText == "" then
+		collisionText = "No collisions"
+	end
 end
 
 function love.draw()
-  love.graphics.clear()
-  
-  -- Draw all objects
-  for _, obj in pairs(objects) do
-    love.graphics.setColor(0.5, 0.5, 1)
-    obj:draw('fill')
-  end
-  
-  -- Draw player
-  love.graphics.setColor(1, 1, 1)
-  player:draw()
-  
-  -- Draw collision text
-  love.graphics.print(collisionText, 10, 10)
+	love.graphics.clear()
+
+	-- Draw all objects
+	for _, obj in pairs(objects) do
+		love.graphics.setColor(0.5, 0.5, 1)
+		obj:draw("fill")
+	end
+
+	-- Draw player
+	love.graphics.setColor(1, 1, 1)
+	player:draw()
+
+	-- Draw collision text
+	love.graphics.print(collisionText, 10, 10)
 end
